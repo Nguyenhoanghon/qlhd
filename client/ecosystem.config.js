34543eb5982d401602b5b33a7a8a0feeef184003
Hoanghon <nhhon110100@gmail.com>
2023-01-11 11:27:41 +0700

    Update

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -1,7 +1,7 @@
 //chưa sữ dụng, nghiên cứu tiếp để deploy
 module.exports = {
   apps : [{
-    name: 'qlhd_hnn',
+    name: 'client',
     script: './src/index.js',
     cwd: __dirname,
     instances: 1,
@@ -19,7 +19,7 @@ module.exports = {
       host : '103.88.121.45',
       ref  : 'origin/main',
       repo : 'git@gitlab.honeynet.vn:63379/outsourcing-projects/qlhd_hnn.git',
-      path : 'home/admin_hh/qlhd_hnn/server',
+      path : 'qlhd_hnn/server',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
